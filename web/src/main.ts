e733de7c8b404ea237569125136d71775f1b75a4
xna <xna00@qq.com>
2021-03-03 10:27:26 +0800

    add scss

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import router from "./router";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import './assets/style.scss'
+
+createApp(App).use(router).mount("#app");
