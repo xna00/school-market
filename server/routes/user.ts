23086bfe6b9993ec4511e687845b41ca96d5a1de
xna <xna00@qq.com>
2021-03-03 20:51:01 +0800

    add route me

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,13 +1,17 @@
 import express, { Express } from "express";
+import auth from "../middleware/auth";
 import User from "../models/User";
 
 export default (app: Express) => {
   const router = express.Router();
 
+  router.get("/me", auth(), async (req: any, res) => {
+    res.send(req.user);
+  });
+
   router.get("/:id", async (req, res) => {
     const user = await User.findById(req.params.id).populate("goods");
     res.send(user);
   });
-
   app.use("/api/users", router);
 };
