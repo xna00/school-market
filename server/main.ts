7ca4ae77c36fe515161bbbe64a18a42d6a18dd38
xna <xna00@qq.com>
2021-03-03 20:51:24 +0800

    socketio

--- server/main.ts
+++ server/main.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 import cors from "cors";
 import errorHandler from "./middleware/errorHandler";
 import db from "./plugins/db";
@@ -7,6 +9,11 @@ import auth from "./routes/auth";
 import user from "./routes/user";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
 
 app.set("secret", "jf389u3cosidufq0e3");
 
@@ -20,6 +27,6 @@ auth(app);
 user(app);
 
 app.use(errorHandler());
-app.listen(4000, () => {
+server.listen(4000, () => {
   console.log("http://localhost:4000");
 });
