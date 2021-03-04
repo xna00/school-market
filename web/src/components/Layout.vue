c6634adad37444204a407a58da7de935aaf2ce2f
xna <xna00@qq.com>
2021-03-05 00:08:13 +0800

    显示未读消息数量

--- web/src/components/Layout.vue
+++ web/src/components/Layout.vue
@@ -11,7 +11,7 @@
         <Icon name="trend" />
         <div>动态</div>
       </router-link>
-      <router-link to="/message">
+      <router-link to="/message" class="message" :count="messageCount">
         <Icon name="message" />
         <div>消息</div>
       </router-link>
@@ -24,6 +24,14 @@
 </template>
 <script lang="ts" setup>
 import Icon from "./Icon";
+import { sessions } from "../lib/message";
+import { computed } from "@vue/runtime-core";
+const messageCount = computed(() =>
+  sessions.value.reduce<number>(
+    (total, current) => total + current.msgs.length,
+    0
+  )
+);
 </script>
 <style lang="scss" scoped>
 main {
@@ -39,6 +47,20 @@ nav {
     }
     &.router-link-active {
       color: sandybrown;
+    }
+    &.message {
+      position: relative;
+      &::after {
+        content: attr(count);
+        position: absolute;
+        background: red;
+        border-radius: 50%;
+        font-size: 1.8vw;
+        top: 0;
+        left: 54%;
+        width: 2.4em;
+        line-height: 2.4em;
+      }
     }
   }
 }
