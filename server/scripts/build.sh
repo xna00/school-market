b21962dab44b1a4b23b8b7a8fe0cf317e0ce5759
xna <xna00@qq.com>
2021-04-01 21:59:55 +0800

    fix upload bug: multerDest

--- server/scripts/build.sh
+++ server/scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm -rf dist
 tsc
-echo '{ "multerDest": "../uploads/", "uploads": "/../uploads", "assets": "/../assets", "web": "/../../web/dist" }' > ./dist/path.json
+echo '{ "multerDest": "uploads/", "uploads": "/../uploads", "assets": "/../assets", "web": "/../../web/dist" }' > ./dist/path.json
