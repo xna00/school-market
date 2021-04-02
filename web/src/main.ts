55d3841d70bad9dfcbd1e33b4c9864ae175b74b0
xna <xna00@qq.com>
2021-04-02 14:12:16 +0800

    add water.css

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,10 +3,11 @@ import router from "./router";
 import App from "./App.vue";
 
 import "./assets/style.scss";
+import "./assets/water.scss";
 
 import Layout from "./components/Layout.vue";
 import Icon from "./components/Icon";
 const app = createApp(App).use(router);
 app.component("Layout", Layout);
-app.component('Icon', Icon)
+app.component("Icon", Icon);
 app.mount("#app");
